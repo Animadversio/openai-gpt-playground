a21ee0b264aaa46bb37e8205ea741c0b308ec1c6
BinxuWang <binxu.wang@wustl.edu>
2023-05-23 19:14:15 -0500

    adding navigation to arxiv

--- notion_arxiv_browse.py
+++ notion_arxiv_browse.py
@@ -93,16 +93,39 @@ def blocks2text(blocks):
 
 
 def fetch_K_results(search_obj, K=10, offset=0):
+    """Fetches K results from the search object, starting from offset, and returns a list of results."""
     results = []
     try:
         for entry in search_obj.results(offset=offset):
             results.append(entry)
             if len(results) >= K:
                 break
-    # except
     except StopIteration:
         pass
     return results
+
+
+def add_to_notion(paper: arxiv.arxiv.Result):
+    title = paper.title
+    arxiv_id = paper.entry_id.split("/")[-1]
+    # check if entry already exists in Notion database
+    results_notion = notion.databases.query(database_id=database_id,
+                                            filter={"property": "Link", "url": {"contains": arxiv_id}})
+    if len(results_notion["results"]) == 0:
+        print(f"Adding entry paper {arxiv_id}: {title}")
+        page_id, page = arxiv_entry2page(database_id, paper)
+        print(f"Added entry {page_id} for arxiv paper {arxiv_id}: {title}")
+        print_entries([page], print_prop=("url",))
+    else:
+        print_entries(results_notion, print_prop=("url",))
+        print("Entry already exists as above. Exiting.")
+        for page in results_notion["results"]:
+            print_entries([page], print_prop=("url",))
+            try:
+                blocks = notion.blocks.children.list(page["id"])
+                blocks2text(blocks)
+            except Exception as e:
+                print(e)
 
 
 # query = "2106.05963"
@@ -110,110 +133,62 @@ MAX_RESULTS = 35
 while True:
     try:
         cnt = 0
-        # query = questionary.text("Enter arXiv ID:").ask()
-        query = session.prompt("Enter arXiv ID: ", multiline=False)
+        query = session.prompt("Enter arXiv ID or query str: ", multiline=False)
         search_obj = arxiv.Search(query, )
-        # results_arxiv = list(search_obj.results(offset=cnt))
         results_arxiv = fetch_K_results(search_obj, K=MAX_RESULTS, offset=cnt)
-        if len(results_arxiv) > 1:
-            while len(results_arxiv) > 1:
+        if len(results_arxiv) == 0:
+            print("No results found.")
+            continue
+        elif len(results_arxiv) == 1:
+            paper = results_arxiv[0]
+            print_arxiv_entry(paper)
+            # Add the entry if confirmed
+            if questionary.confirm("Add this entry?").ask():
+                add_to_notion(paper)
+        elif len(results_arxiv) > 1:
+            # multiple results found, complex logic to navigate through results
+            last_selection = None  # last selected result to highlight
+            while True:
+                # looping of results and pages, navigating through search results
                 print("Multiple results found. Please select one:")
-                # for i, paper in enumerate(results_arxiv):
-                #     print(f"{i + 1}: {paper.title}")
-                # selection = questionary.select("Select paper:", choices=[str(i + 1) for i in range(len(results_arxiv))]).ask()
                 choices = [f"{i + 1}: [{paper.entry_id.split('/')[-1]}] {paper.title} " for i, paper in enumerate(results_arxiv)]
-                choices.append("0: Next page")
-                selection = questionary.select("Select paper:", choices=choices).ask()
+                if len(results_arxiv) == MAX_RESULTS:
+                    choices.append("0: Next page")
+                if cnt > 0:
+                    choices.append("-1: Prev page")
+                selection = questionary.select("Select paper:", choices=choices, default=None if last_selection is None
+                                               else choices[last_selection]).ask()
                 selection = int(selection.split(":")[0])
                 if selection == 0:
                     cnt += MAX_RESULTS
                     results_arxiv = fetch_K_results(search_obj, K=MAX_RESULTS, offset=cnt)
-                    # results_arxiv = list(search_obj.results(offset=cnt))
+                    continue
+                if selection == -1:
+                    cnt -= MAX_RESULTS
+                    results_arxiv = fetch_K_results(search_obj, K=MAX_RESULTS, offset=cnt)
                     continue
                 else:
                     paper = results_arxiv[int(selection) - 1]
-                    break
-        elif len(results_arxiv) == 1:
-            paper = results_arxiv[0]
-        else:
-            print("No results found.")
-            continue
+                    last_selection = int(selection) - 1
+                    print_arxiv_entry(paper)
+                    if questionary.confirm("Add this entry?").ask():
+                        # Add the entry if confirmed
+                        add_to_notion(paper)
+                    # if questionary.confirm("Back to the list").ask():
+                    #     continue
+                    # else:
+                    #     break
 
-        title = paper.title
-        authors = [author.name for author in paper.authors]
-        pubyear = paper.published
-        abstract = paper.summary  # arxiv.download(paper['id'], prefer_source_tex=False).summary
-        arxiv_id = paper.entry_id.split("/")[-1]
-        abs_url = paper.entry_id
-        print_arxiv_entry(paper)
-        # Add the entry if confirmed
-        if not questionary.confirm("Add this entry?").ask():
-            continue
-        # check if entry already exists in Notion database
-        results_notion = notion.databases.query(database_id=database_id,
-                              filter={"property": "Link", "url": {"contains": arxiv_id}})
-        if len(results_notion["results"]) == 0:
-            print(f"Adding entry paper {arxiv_id}: {title}")
-            page_id, page = arxiv_entry2page(database_id, paper)
-            print(f"Added entry {page_id} for arxiv paper {arxiv_id}: {title}")
-            print_entries([page], print_prop=("url", ))
-        else:
-            print_entries(results_notion, print_prop=("url", ))
-            print("Entry already exists as above. Exiting.")
-            for page in results_notion["results"]:
-                print_entries([page], print_prop=("url", ))
-                try:
-                    blocks = notion.blocks.children.list(page["id"])
-                    blocks2text(blocks)
-                except Exception as e:
-                    print(e)
-            continue
+        # title = paper.title
+        # authors = [author.name for author in paper.authors]
+        # pubyear = paper.published
+        # abstract = paper.summary  # arxiv.download(paper['id'], prefer_source_tex=False).summary
+        # arxiv_id = paper.entry_id.split("/")[-1]
+        # abs_url = paper.entry_id
+        # print_arxiv_entry(paper)
+        # # Add the entry if confirmed
+        # if questionary.confirm("Add this entry?").ask():
+        #     add_to_notion(paper)
+        # back2list = questionary.confirm("Back to the list").ask()
     except Exception as e:
         continue
-#%%
-
-
-# blocks2text(blocks)
-#%% Scratch zone
-# page_prop = {
-#     'Name': {
-#         "title": [
-#             {
-#                 "text": {
-#                     "content": f"[{arxiv_id}] {title}"
-#                 }
-#             }],
-#     },
-#     "Author": {
-#                 "multi_select": [
-#                     {'name': name} for name in authors
-#                 ]
-#             },
-#     'Publishing/Release Date': {
-#                'date': {'start': pubyear.date().isoformat(), }
-#     },
-#     'Link': {
-#                'url': abs_url
-#     }
-# }
-# content_block = [{'quote': {"rich_text": [{"text": {"content": abstract}}]}},
-#                  {'heading_2': {"rich_text": [{"text": {"content": "Related Work"}}]}},
-#                  {'paragraph': {"rich_text": [{"text": {"content": ""}}]}},
-#                  {'heading_2': {"rich_text": [{"text": {"content": "Techniques"}}]}},
-#                  {'paragraph': {"rich_text": [{"text": {"content": ""}}]}},
-#                  ]
-#
-# new_page = notion.pages.create(parent={"database_id": database_id}, properties=page_prop)
-# notion.blocks.children.append(new_page["id"], children=content_block)
-# #%%
-# results_notion = notion.databases.query(database_id, filter={"property": "Link", "url": {"is_not_empty": True}})
-#%%
-# %%
-# # unzip .tar.gz file
-# import tarfile
-# import os
-# import shutil
-# tarfile.open('./2106.05963v3.Learning_to_See_by_Looking_at_Noise.tar.gz').extractall()
-
-#%%
-notion.blocks.children.list('767b3f0b-f4f6-4f55-81c6-3a2c7b8a8b23')
